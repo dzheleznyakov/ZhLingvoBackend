19ba4a363095e3a9cd697e31b54fcd3d7f68e586
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-05-14 17:14:31 +0100

    Basic server configuration for a single page app + API

--- src/main/java/zh/lingvo/Main.java
+++ src/main/java/zh/lingvo/Main.java
@@ -1,61 +1,43 @@
 package zh.lingvo;
 
-import org.eclipse.jetty.rewrite.handler.RedirectPatternRule;
-import org.eclipse.jetty.rewrite.handler.RewriteHandler;
-import org.eclipse.jetty.rewrite.handler.RewritePatternRule;
-import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class Main extends AbstractHandler {
-    @Override
-    public void handle(
-            String target,
-            Request baseRequest,
-            HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
-        response.setContentType("text/html; charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello World<h2>");
-        baseRequest.setHandled(true);
-    }
+public class Main {
 
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
+
+        ServletContextHandler context0 = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context0.setContextPath("/ctx0");
+        server.setHandler(context0);
+
+        context0.addServlet(new ServletHolder(new HelloServlet()), "/*");
+        context0.addServlet(new ServletHolder(new HelloServlet("Buongiorno Mondo")), "/it/*");
+        context0.addServlet(new ServletHolder(new HelloServlet("Bunjour le Monde")), "/fr/*");
+
+
+        WebAppContext webapp = new WebAppContext();
         String resourceBase = "src/main/resources/public";
+        webapp.setContextPath("/*");
+        webapp.setResourceBase(resourceBase);
+        webapp.setParentLoaderPriority(true);
+        webapp.setErrorHandler(new MyErrorHandler());
 
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase(resourceBase);
-        resourceHandler.setDirectoriesListed(true);
-        resourceHandler.setDirAllowed(true);
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        contexts.setHandlers(new Handler[] { context0, webapp });
 
-        ContextHandler contextHandler = new ContextHandler(".");
-        contextHandler.setHandler(resourceHandler);
+        server.setHandler(contexts);
 
-        ContextHandler contextHandler1 = new ContextHandler("./language");
-        contextHandler.setHandler(resourceHandler);
-
-//        RewriteHandler rewriteHandler = new RewriteHandler();
-//        rewriteHandler.setRewriteRequestURI(true);
-//        rewriteHandler.setRewritePathInfo(true);
-//
-//        RewritePatternRule redirectRule = new RewritePatternRule();
-//        redirectRule.setPattern("/dictionary");
-//        redirectRule.setReplacement(".");
-//        rewriteHandler.addRule(redirectRule);
-
-
-        server.setHandler(contextHandler);
-        server.setHandler(contextHandler1);
-//        server.setHandler(rewriteHandler);
-
-        server.start();
-        server.join();
+        try {
+            server.start();
+            server.join();
+        } finally {
+            server.destroy();
+        }
     }
 }
