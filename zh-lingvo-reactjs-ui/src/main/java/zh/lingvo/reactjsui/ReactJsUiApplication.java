41f6cbd56a4a60a5fa2e6585caab692c95405336
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-02-08 00:11:28 +0000

    Create SQLiteDialect

---
+++ zh-lingvo-reactjs-ui/src/main/java/zh/lingvo/reactjsui/ReactJsUiApplication.java
@@ -0,0 +1,13 @@
+package zh.lingvo.reactjsui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("zh.lingvo")
+public class ReactJsUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactJsUiApplication.class, args);
+    }
+}
