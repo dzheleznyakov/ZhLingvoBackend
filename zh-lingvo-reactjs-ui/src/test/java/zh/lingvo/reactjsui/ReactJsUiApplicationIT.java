b73814383b60fa0f290bc5616f62360888739ab3
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-02-10 01:56:13 +0000

    Set up UI ReactJs app. Closes #12

---
+++ zh-lingvo-reactjs-ui/src/test/java/zh/lingvo/reactjsui/ReactJsUiApplicationIT.java
@@ -0,0 +1,11 @@
+package zh.lingvo.reactjsui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJsUiApplicationIT {
+    @Test
+    void contextLoads() {
+    }
+}
