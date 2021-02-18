a8c8da5c1b9f9b2cd7954ddba3ca0c7103b950dd
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-02-18 01:58:22 +0000

    Size layout and add Toolbar component. #34

---
+++ zh-lingvo-reactjs-ui/src/main/app/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Toolbar.module.scss';
+
+const Toolbar = props => {
+    return (
+        <div className={classes.Toolbar}>Toolbar</div>
+    );
+};
+
+Toolbar.propTypes = {};
+
+Toolbar.defaultProps = {};
+
+export default Toolbar;
--- zh-lingvo-reactjs-ui/src/main/app/src/hoc/Layout/Layout.js
+++ zh-lingvo-reactjs-ui/src/main/app/src/hoc/Layout/Layout.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 import classes from './Layout.module.scss';
 
+import Toolbar from '../../components/Toolbar/Toolbar';
+
 const Layout = props => {
     const { children } = props;
 
     return (
         <div className={classes.Layout}>
-            <div className={classes.Toolbar}>Toolbar</div>
+            <div className={classes.Toolbar}><Toolbar /></div>
             <div className={classes.Breadcrumb}>Breadcrumb</div>
             <div className={classes.Content}>
                 {children}
