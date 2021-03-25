5eafce0cc818733e269c1fe1ccbd5077cb694f2d
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-03-25 13:55:27 +0000

    Fix ReactJsUiApplicationIT .#43

--- zh-lingvo-data/src/main/java/zh/lingvo/data/constants/Profiles.java
+++ zh-lingvo-data/src/main/java/zh/lingvo/data/constants/Profiles.java
@@ -2,4 +2,5 @@ package zh.lingvo.data.constants;
 
 public interface Profiles {
     String DEV = "dev";
+    String IGNORE = "@never-ever@";
 }
--- zh-lingvo-data/src/main/java/zh/lingvo/data/fixtures/SubWordRepository.java
+++ zh-lingvo-data/src/main/java/zh/lingvo/data/fixtures/SubWordRepository.java
@@ -1,7 +1,11 @@
 package zh.lingvo.data.fixtures;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+import static zh.lingvo.data.constants.Profiles.IGNORE;
+
+@Profile(IGNORE)
 public interface SubWordRepository<T, ID> extends CrudRepository<T, ID> {
     Class<T> getSubWordClass();
 }
