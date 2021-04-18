2b902453112153085797637cab48f16edfad577c
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-04-18 02:42:01 +0100

    Style WordView in editing mode. #27

--- zh-lingvo-reactjs-ui/src/main/app/src/components/DictionaryView/WordCard/WordView/WordView.js
+++ zh-lingvo-reactjs-ui/src/main/app/src/components/DictionaryView/WordCard/WordView/WordView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import classes from './WordView.module.scss';
@@ -7,17 +8,23 @@ import { wordType } from './wordTypes';
 import { Transcription } from './SubWordParts';
 import SemanticBlock from './SubWordParts/SemanticBlock';
 import toRoman from '../../../../utils/toRomanNumbers';
+import { isEditingSelector } from '../../../../store/selectors';
 
 const WordView = props => {
     const { index, word } = props;
+    const isEditing = useSelector(isEditingSelector);
 
     const semBlocks = word.semBlocks && word.semBlocks
         .map((sb, i) => <SemanticBlock key={sb.id} semBlock={sb} index={i} />);
     
     const path = [`${index}`];
 
+    const wrapperClasses = [];
+    if (isEditing)
+        wrapperClasses.push(classes.Editing);
+
     return (
-        <div>
+        <div className={wrapperClasses.join(' ')}>
             <div className={classes.WordEnum}>{toRoman(index + 1)}</div>
             <Transcription parentPath={path}>{word.transcription}</Transcription>
             <div>
