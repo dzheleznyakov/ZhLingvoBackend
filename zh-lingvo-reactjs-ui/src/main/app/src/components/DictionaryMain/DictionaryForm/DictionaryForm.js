199dabc387721cda4b65872d8f1f22726d64bee6
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2021-04-21 14:01:32 +0100

    Use the generic Form instead of ad hoc ones. #41

--- zh-lingvo-reactjs-ui/src/main/app/src/components/DictionaryMain/DictionaryForm/DictionaryForm.js
+++ zh-lingvo-reactjs-ui/src/main/app/src/components/DictionaryMain/DictionaryForm/DictionaryForm.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-import classes from './DictionaryForm.module.scss';
-
-import { Dialog, Error } from '../../UI';
+import { Form, formInputTypes, validators } from '../../UI';
 import * as actions from '../../../store/actions';
 import { dictionaryType } from '../propTypes';
 import { languagesSelector } from '../../../store/selectors';
@@ -12,66 +10,60 @@ import { useActionOnMount, useAutofocus } from '../../../hooks';
 
 const DictionaryForm = props => {
     const { close, confirmed, title, dictionary, disabledInputs } = props;
-    const [errorMessages, setErrorMessages] = useState({});
     const languages = useSelector(languagesSelector);
-    const nameInputRef = useRef();
-    const languageSelectRef = useRef();
 
     useActionOnMount(actions.fetchAllLanguages());
-    
-    const verify = () => {
-        const updatedErrors = { ...errorMessages };
-        const name = nameInputRef && nameInputRef.current.value;
-        if (!name || name.trim().length < 3)
-            updatedErrors.name = 'Dictionary name should be of length at least 3';
-        else
-            updatedErrors.name && delete updatedErrors.name;
-        setErrorMessages(updatedErrors);
-    }
-
-    useEffect(() => {
-        nameInputRef && verify();
-    }, [nameInputRef]);
-
-    useAutofocus(nameInputRef);
-
-    const onConfirm = () => {
-        const name = nameInputRef.current.value;
-        const language = languages.find(lang => lang.name === languageSelectRef.current.value);
-        confirmed(name, language)
-    };
 
     const defaultName = dictionary.name;
     const defaultLanguage = dictionary.language.name;
 
-    const errors = Object.keys(errorMessages)
-        .map(key => <Error key={`err-${key}`} message={errorMessages[key]} />)
+    const dictionaryGroup = {
+        key: 'dictionary',
+        label: title,
+    };
+
+    const nameRef = useRef();
+    const dicNameField = {
+        key: 'name',
+        label: 'Dictionary name',
+        type: formInputTypes.TEXT,
+        defaultValue: defaultName || '',
+        groupKey: dictionaryGroup.key,
+        forwardRef: nameRef,
+        disabled: disabledInputs.name,
+        validation: [{
+            validate: validators.minLength(3),
+            failureMessage: 'Dicitonary name should contain at least 3 characters',
+        }],
+    };
+
+    const langRef = useRef();
+    const langField = {
+        key: 'lang',
+        label: 'Language',
+        type: formInputTypes.SELECT,
+        defaultValue: defaultLanguage || '',
+        values: languages.map(({ name }) => name),
+        groupKey: dictionaryGroup.key,
+        forwardRef: langRef,
+        disabled: disabledInputs.language,
+    };
+
+    useAutofocus(nameRef);
+
+    const onConfirm = () => {
+        const name = nameRef.current.value;
+        const language = languages.find(lang => lang.name === langRef.current.value);
+        confirmed(name, language)
+    };
 
     return (
-        <Dialog close={close} confirmed={onConfirm}>
-            <fieldset className={classes.DictionaryForm}>
-                <legend>{title}</legend>
-                <div className={classes.FieldsWrapper}>
-                    <label>Dictionary name:</label>
-                    <input 
-                        type="text" 
-                        ref={nameInputRef} 
-                        onKeyUp={verify} 
-                        defaultValue={defaultName}
-                        disabled={disabledInputs.name}
-                    />
-                    <label>Language:</label>
-                    <select 
-                        ref={languageSelectRef} 
-                        defaultValue={defaultLanguage}
-                        disabled={disabledInputs.language}
-                    >
-                        {languages.map(lang => <option key={lang.code}>{lang.name}</option>)}
-                    </select>
-                </div>
-            </fieldset>
-            {errors}
-        </Dialog>
+        <Form 
+            fields={[dicNameField, langField]}
+            groups={[dictionaryGroup]}
+            canceled={close}
+            confirmed={onConfirm}
+        />
     );
 };
 
