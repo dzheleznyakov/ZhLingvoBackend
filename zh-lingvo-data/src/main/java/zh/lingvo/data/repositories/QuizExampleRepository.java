f661dcefdb72fab6b10659ebb2ab33700e4ffaf3
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-02-25 02:08:50 +0000

    Implement QuizExampleRepository. Closes #73

---
+++ zh-lingvo-data/src/main/java/zh/lingvo/data/repositories/QuizExampleRepository.java
@@ -0,0 +1,7 @@
+package zh.lingvo.data.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import zh.lingvo.data.model.QuizExample;
+
+public interface QuizExampleRepository extends CrudRepository<QuizExample, Long> {
+}
