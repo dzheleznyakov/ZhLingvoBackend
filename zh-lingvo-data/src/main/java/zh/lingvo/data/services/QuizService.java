9153bbec8ca2918e0f8bf6c7c7a0c1b59c602189
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-05-07 21:24:07 +0100

    QuizService. #76

---
+++ zh-lingvo-data/src/main/java/zh/lingvo/data/services/QuizService.java
@@ -0,0 +1,17 @@
+package zh.lingvo.data.services;
+
+import zh.lingvo.data.model.Quiz;
+import zh.lingvo.data.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuizService {
+    Optional<Quiz> findById(Long id, User user);
+
+    List<Quiz> findAll(User user);
+
+    Optional<Quiz> save(Quiz quiz, User user);
+
+    boolean deleteById(Long id, User user);
+}
