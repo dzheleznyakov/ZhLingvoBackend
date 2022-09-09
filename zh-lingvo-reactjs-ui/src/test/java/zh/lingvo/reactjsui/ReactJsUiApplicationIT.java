3d38988177c5500ae2b16b7f8f6cd9d2e4399a7a
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-09-09 20:42:18 +0100

    Disable ReactJsUiApplicationIT

--- zh-lingvo-reactjs-ui/src/test/java/zh/lingvo/reactjsui/ReactJsUiApplicationIT.java
+++ zh-lingvo-reactjs-ui/src/test/java/zh/lingvo/reactjsui/ReactJsUiApplicationIT.java
@@ -1,11 +1,12 @@
 package zh.lingvo.reactjsui;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReactJsUiApplicationIT {
+    /*
     @Test
     void contextLoads() {
     }
+    */
 }
