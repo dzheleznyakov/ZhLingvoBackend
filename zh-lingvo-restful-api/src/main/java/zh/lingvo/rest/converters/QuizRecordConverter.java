a76c0f27b7fdd591c88295abc7629c7d655b3a62
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2022-12-28 02:36:06 +0000

    UI tutor: Meaning to quiz record: API to create a quiz record from the meaning #108

--- zh-lingvo-restful-api/src/main/java/zh/lingvo/rest/converters/QuizRecordConverter.java
+++ zh-lingvo-restful-api/src/main/java/zh/lingvo/rest/converters/QuizRecordConverter.java
@@ -1,13 +1,23 @@
 package zh.lingvo.rest.converters;
 
+import com.google.common.collect.ImmutableSet;
 import org.springframework.stereotype.Component;
 import zh.lingvo.core.domain.PartOfSpeech;
+import zh.lingvo.data.model.Example;
+import zh.lingvo.data.model.Meaning;
+import zh.lingvo.data.model.QuizExample;
 import zh.lingvo.data.model.QuizRecord;
+import zh.lingvo.data.model.QuizTranslation;
+import zh.lingvo.data.model.SemanticBlock;
+import zh.lingvo.data.model.Translation;
+import zh.lingvo.data.model.Word;
 import zh.lingvo.rest.commands.QuizRecordCommand;
 import zh.lingvo.rest.commands.QuizRecordOverviewCommand;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
 
 @Component
 public class QuizRecordConverter {
@@ -18,8 +28,7 @@ public class QuizRecordConverter {
     public QuizRecordConverter(
             QuizRecordToQuizRecordOverviewCommand recordToOverviewCommand,
             QuizRecordToQuizRecordCommand recordToCommand,
-            QuizRecordCommandToQuizRecord commandToRecord
-    ) {
+            QuizRecordCommandToQuizRecord commandToRecord) {
         this.recordToOverviewCommand = recordToOverviewCommand;
         this.recordToCommand = recordToCommand;
         this.commandToRecord = commandToRecord;
@@ -54,5 +63,38 @@ public class QuizRecordConverter {
         if (value == null)
             value = fallback.get();
         setter.accept(value);
+    }
+
+    public QuizRecord createQuizRecord(Meaning meaning) {
+        SemanticBlock semBlock = meaning.getSemBlock();
+        Word word = semBlock.getWord();
+
+        QuizRecord quizRecord = new QuizRecord();
+        quizRecord.setWordMainForm(word.getMainForm());
+        quizRecord.setTranscription(word.getTranscription());
+        quizRecord.setPos(semBlock.getPos());
+
+        ImmutableSet<QuizTranslation> quizTranslations = firstNonNull(meaning.getTranslations(), ImmutableSet.<Translation>of())
+                .stream()
+                .map(translation -> QuizTranslation.builder()
+                        .value(translation.getValue())
+                        .elaboration(translation.getElaboration())
+                        .record(quizRecord)
+                        .build())
+                .collect(ImmutableSet.toImmutableSet());
+        quizRecord.setTranslations(quizTranslations);
+
+        ImmutableSet<QuizExample> quizExamples = firstNonNull(meaning.getExamples(), ImmutableSet.<Example>of())
+                .stream()
+                .map(example -> QuizExample.builder()
+                        .remark(example.getRemark())
+                        .expression(example.getExpression())
+                        .explanation(example.getExplanation())
+                        .record(quizRecord)
+                        .build())
+                .collect(ImmutableSet.toImmutableSet());
+        quizRecord.setExamples(quizExamples);
+
+        return quizRecord;
     }
 }
