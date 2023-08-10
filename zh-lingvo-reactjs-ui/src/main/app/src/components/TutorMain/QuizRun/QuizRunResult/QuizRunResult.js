2eaa07e0730e8c4db90f435fed230ece299701bb
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-08-10 18:44:54 +0100

    Tutor UI: Add 'Back to quiz' button on the quiz run result screen. #115

--- zh-lingvo-reactjs-ui/src/main/app/src/components/TutorMain/QuizRun/QuizRunResult/QuizRunResult.js
+++ zh-lingvo-reactjs-ui/src/main/app/src/components/TutorMain/QuizRun/QuizRunResult/QuizRunResult.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import classes from './QuizRunResult.module.scss';
 
@@ -8,11 +8,13 @@ import { allQuizRecordsSelector, quizRunSelector } from '../../../../store/selec
 import { CollapsableListView } from '../../../UI';
 import { useActionOnMount } from '../../../../hooks';
 import * as actions from '../../../../store/actions';
+import { TUTOR_QUIZ } from '../../../../static/constants/paths';
 
 const QuizRunResult = () => {
     const { qid } = useParams();
     const quizRun = useSelector(quizRunSelector);
     const records = useSelector(allQuizRecordsSelector) || [];
+    const dispatch = useDispatch();
 
     useActionOnMount(actions.completeQuizRun(quizRun, qid));
 
@@ -39,6 +41,16 @@ const QuizRunResult = () => {
                     };
                 })
             : [];
+    
+    const onBackToQuizClicked = () => {
+        const url = TUTOR_QUIZ.replace(/:(\w)+\\?/g, param => {
+            switch (param) {
+                case ':qid': return qid;
+                case ':rid': return '';
+            }
+        })
+        dispatch(actions.navigateTo(url));
+    };
 
     const summary = qid && quizRun && records.length && (
         <div className={classes.QuizRunResultContent}>
@@ -46,6 +58,10 @@ const QuizRunResult = () => {
             {quizRun.doneRecords.length}
             <CollapsableListView heading='Correct answers:' items={correctAswers} />
             <CollapsableListView heading='Incorrect answers:' items={incorrectAnswers} />
+            <button 
+                className={classes.BackButton}
+                onClick={onBackToQuizClicked}
+            >Back to quiz</button>
         </div>
     );
 
