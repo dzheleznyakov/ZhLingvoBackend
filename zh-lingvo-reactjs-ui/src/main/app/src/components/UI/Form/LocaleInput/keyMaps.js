62160d8144b1ebed733188f78c7ed214d6dee615
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-12-31 23:29:59 +0000

    Add Portuguese (Br) keyboard remapper. Closes #132

--- zh-lingvo-reactjs-ui/src/main/app/src/components/UI/Form/LocaleInput/keyMaps.js
+++ zh-lingvo-reactjs-ui/src/main/app/src/components/UI/Form/LocaleInput/keyMaps.js
@@ -1,7 +1,9 @@
 import enMap from './enMap';
 import esMap from './esMap';
 import ruMap from './ruMap';
+import ptMap from './ptMap';
 
 export const En = enMap;
 export const Es = esMap;
 export const Ru = ruMap;
+export const Pt = ptMap;
